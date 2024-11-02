304fe6ca561ab7298361bab0d23cf99f91ee4863
Alexander <167740+abl@users.noreply.github.com>
2024-11-02 21:39:38 +0000

    implement /budget and dependencies

--- src/test/java/edu/northeastern/cs5500/starterbot/controller/UserPreferenceControllerTest.java
+++ src/test/java/edu/northeastern/cs5500/starterbot/controller/UserPreferenceControllerTest.java
@@ -1,6 +1,7 @@
 package edu.northeastern.cs5500.starterbot.controller;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.fail;
 
 import edu.northeastern.cs5500.starterbot.repository.InMemoryRepository;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,7 @@ class UserPreferenceControllerTest {
     static final String USER_ID_2 = "2kjfksdjdkhokljhaoe";
     static final String PREFERRED_NAME_1 = "Joe";
     static final String PREFERRED_NAME_2 = "Penny";
+    static final Integer BUDGET_1 = 1000;
 
     private UserPreferenceController getUserPreferenceController() {
         UserPreferenceController userPreferenceController =
@@ -58,5 +60,38 @@ class UserPreferenceControllerTest {
                 .isEqualTo(PREFERRED_NAME_1);
         assertThat(userPreferenceController.getPreferredNameForUser(USER_ID_2))
                 .isEqualTo(PREFERRED_NAME_2);
+    }
+
+    @Test
+    void testSetBudgetActuallySetsBudget() {
+        // setup
+        UserPreferenceController userPreferenceController = getUserPreferenceController();
+
+        // precondition
+        assertThat(userPreferenceController.getMaximumBudgetForUser(USER_ID_1))
+                .isNotEqualTo(BUDGET_1);
+
+        // mutation
+        userPreferenceController.setMaximumBudgetForUser(USER_ID_1, BUDGET_1);
+
+        // postcondition
+        assertThat(userPreferenceController.getMaximumBudgetForUser(USER_ID_1)).isEqualTo(BUDGET_1);
+    }
+
+    @Test
+    void testThatBudgetMustBeNonnegative() {
+        // setup
+        UserPreferenceController userPreferenceController = getUserPreferenceController();
+
+        // precondition
+        assertThat(userPreferenceController.getMaximumBudgetForUser(USER_ID_1)).isNull();
+
+        // mutation
+        try {
+            userPreferenceController.setMaximumBudgetForUser(USER_ID_1, -1);
+            fail("Expected IllegalArgumentException was not thrown");
+        } catch (IllegalArgumentException e) {
+            assertThat(e.getMessage()).isEqualTo("maximumBudget");
+        }
     }
 }
